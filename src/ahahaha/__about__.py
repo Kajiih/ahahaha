5de693a38bd5466d008a17cb4e9f3e61385d0afe
Kajih <itskajih@gmail.com>
2025-06-20 22:23:09 +0200

    bump: version 0.12.0 → 0.13.0

--- src/ahahaha/__about__.py
+++ src/ahahaha/__about__.py
@@ -1,7 +1,7 @@
 """ahahaha metadata."""
 
 __module_name__ = "ahahaha"  # Not dynamic
-__version__ = "0.12.0"
+__version__ = "0.13.0"
 
 __authors__ = ["Julian Paquerot"]
 __author_emails__ = ["itskajih@gmail.com"]
